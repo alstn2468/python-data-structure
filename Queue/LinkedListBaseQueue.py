ede690c18ed5f7912abe7853378f0efa3be36486
alstn2468 <alstn2468_@naver.com>
2018-06-20 00:15:21 +0900

    2018-06-20

---
+++ Queue/LinkedListBaseQueue.py
@@ -0,0 +1,25 @@
+# LinkedListBaseQueue.py
+
+class Node :
+
+    def __init__(self, data) :
+        self.data = data
+        self.next = none
+
+class LinkedListBaseQueue :
+
+    def __init__(self) :
+        self.front = none
+        self.rear = none
+
+    def enqueue(self, data) :
+
+    def dequeue(self) :
+
+    def peek(self) :
+
+    def is_empty(self) :
+
+    def display(self) :
+
+if __name__ == '__main__' :
