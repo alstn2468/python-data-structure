b6b4bc0aba981bf3e83758536067fa3850429512
alstn2468 <alstn2468_@naver.com>
2018-06-21 15:44:44 +0900

    2018-06-21

--- Queue/CircularQueue.py
+++ Queue/CircularQueue.py
@@ -11,21 +11,50 @@ class CircularQueue :
 
     # 배열 기반 원형 큐 삽입 함수
     def enqueue(self, data) :
+        if self.is_full() :
+            raise Exception('QUEUE FULL ERROR!')
+
+        else :
+            self.rear = (self.rear + 1) % self.max_length
+            self.items[self.rear] = data
 
     # 배열 기반 원형 큐 삭제 함수
     def dequeue(self) :
+        if self.is_empty() :
+            raise Exception('QUEUE EMTPY ERROR!')
+
+        else :
+            self.front = (self.front + 1) % self.max_length
+            del_data = self.items[self.front]
+
+            return del_data
 
     # 배열 기반 원형 큐 참조 함수
     def peek(self) :
+        return self.items[(self.front + 1) % self.max_length]
 
     # 배열 기반 원형 큐 공백 확인 함수
     def is_empty(self) :
+        return self.front == self.rear
 
     # 배열 기반 원형 큐 포화 확인 함수
     def is_full(self) :
+        return self.front == ((self.rear + 1) % self.max_length)
 
     # 배열 기반 원형 큐 출력 함수
     def display(self) :
+        if self.is_empty() :
+            raise Exception('QUEUE EMPTY ERROR!')
+
+        else :
+
+            for i in range(self.front + 1, self.rear + 1) :
+
+                if i == self.rear :
+                    print('%3d' % self.items[i])
+
+                else:
+                    print('%3d <- ' % self.items[i], end = "")
 
 
 if __name__ == '__main__' :
@@ -63,13 +92,15 @@ if __name__ == '__main__' :
     '''
 
     # 배열 기반 원형 큐 상태 출력
-    print('Front : %d' % self.front)
-    print('Rear : %d' % self.rear)
+    print('Front : %d' % queue.front)
+    print('Rear : %d' % queue.rear)
+    print('Peek : %d' % queue.peek())
     print('Is Empty : %s' % queue.is_empty())
     print('Is Full : %s' % queue.is_full())
     '''
     Front : 1
     Rear : 6
+    Peek : 20
     Is Empty : False
     Is Full : False
     '''
