ff48b9eb2638781d978bf0842135481d4c384175
alstn2468 <alstn2468_@naver.com>
2018-06-25 15:58:40 +0900

    2018-06-25

---
+++ Tree/BinaryTree.py
@@ -0,0 +1,12 @@
+# BinaryTree.py
+
+# 이진 트리 노드 클래스
+class Node :
+
+    def __init__(self, data) :
+        self.rnode = None
+        self.lnode = None
+        slef.data = data
+
+# 이진 트리 클래스
+class BinaryTree :
