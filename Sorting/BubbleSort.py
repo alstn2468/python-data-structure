c961be6e8fd36e644c951caf77ab6051cbb77821
alstn2468 <alstn2468_@naver.com>
2018-08-01 14:48:36 +0900

    2018-08-01

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,28 @@
+# BubbleSort.py
+'''
+버블정렬 (BubbleSort)
+O(n²)
+'''
+def BubbleSort(list) :
+    for i in range(len(list)) :
+
+        for j in range(len(list)) :
+            if list[i] < list[j] :
+                temp = list[i]
+                list[i] = list[j]
+                list[j] = temp
+
+if __name__ == '__main__' :
+    # 정렬할 리스트 선언
+    list = [1, 5, 4, 3, 2, 6, 8, 7, 9, 10]
+
+    # 정렬 전 데이터 출력
+    print('- Before Bubble Sorting -')
+    print(list)
+
+    # 데이터 정렬
+    BubbleSort(list)
+
+    # 정렬 후 데이터 출력
+    print('- After Bubble Sorting -')
+    print(list)
