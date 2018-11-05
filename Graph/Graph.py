569636b2c45825c5755b32d873555edddc510297
alstn2468 <alstn2468_@naver.com>
2018-11-05 22:46:16 +0900

    Add Graph.py

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -1,13 +1,29 @@
 # Graph.py
 
-
+# 그래프 클래스 선언
 class Graph :
 
-    def __init__ :
-        pass
+    def __init__(self, graph_dict = None) :
+        if graph_dict is None :
+            graph_dict = {}
 
-    def insert_vertex(v) :
-        pass
+        self.graph_dict = graph_dict
 
-    def insert_edge(u, v) :
-        pass
+    # 그래프 정점 삽입 함수
+    def insert_vertex(self, vertex) :
+        if vertex not in self.graph_dict :
+            self.graph_dict[vertex] = []
+
+    # 그래프 간선 삽입 함수
+    def insert_edge(self, edge) :
+        edge = set(edge)
+
+        (vertex1, vertex2) = tuple(edge)
+
+        if vertex1 in self.graph_dict :
+            self.graph_dict[vertex1].append(vertex2)
+
+        else :
+            self.graph_dict[vertex2] = [vertex2]
+
+if __name__ == '__main__' :
