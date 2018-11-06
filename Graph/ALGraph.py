b57588c873e3974e1203dde04c51b50361ce5ea1
alstn2468 <alstn2468_@naver.com>
2018-11-06 12:02:13 +0900

    Add Vertex Class in ALGraph.py

--- Graph/ALGraph.py
+++ Graph/ALGraph.py
@@ -1,29 +1,70 @@
 # ALGraph.py
 
-# 그래프 클래스 선언
+# 인접 리스트 그래프 정점 클래스
+class Vertex :
+
+    # 입접 리스트 그래프 정점 초기화 함수
+    def __init__(self, vertex) :
+        self.vertex = vertex
+        self.adj_list = []
+
+    # 인접 리스트 그래프 인접리스트 삽입 함수
+    def insert_neighbor(self, neighbor) :
+        if isinsatnce(neighbor, Vertex) :
+            if neighbor.name not in self.adj_list :
+                self.adj_list.append(neighbor.name)
+                neighbor.adj_list.append(self.name)
+
+                self.adj_list = sorted(self.adj_list)
+                neighbor.adj_list = sorted(neighbor.vertex)
+
+        else :
+            return False
+
+    # 인접 리스트 그래프 인접리스트 다중 삽입 함수
+    def insert_neighbors(self, neighbors) :
+        for neighbor in neighbors :
+            if isinsatnce(neighbor, Vertex) :
+                if neighbor.name not in self.adj_list :
+                    self.adj_list.append(neighbor.name)
+                    neighbor.adj_list.append(self.name)
+
+                    self.adj_list = sorted(self.adj_list)
+                    neighbor.adj_list = sorted(neighbor.adj_list)
+
+            else :
+                return False
+
+# 인접 리스트 그래프 클래스
 class Graph :
 
-    def __init__(self, graph_dict = None) :
-        if graph_dict is None :
-            graph_dict = {}
-
-        self.graph_dict = graph_dict
+    # 인접 리스트 그래프 초기화 함수
+    def __init__(self, max_vertex) :
+        self.vertices = {}
 
     # 그래프 정점 삽입 함수
     def insert_vertex(self, vertex) :
-        if vertex not in self.graph_dict :
-            self.graph_dict[vertex] = []
+        pass
+
+    # 그래프 정점 다중 삽입 함수
+    def insert_vertices(self, vertices) :
+        pass
 
     # 그래프 간선 삽입 함수
     def insert_edge(self, edge) :
-        edge = set(edge)
+        pass
 
-        (vertex1, vertex2) = tuple(edge)
+    # 그래프 간선 다중 삽입 함수
+    def insert_edges(self, edges) :
+        pass
 
-        if vertex1 in self.graph_dict :
-            self.graph_dict[vertex1].append(vertex2)
+    # 그래프 출력 함수
+    def display(self) :
+        if len(self.vertices) >= 1 :
+            print(str([str(key) + " : " +
+                       str(self.vertices[key]) for key in self.vertices.keys()]))
 
         else :
-            self.graph_dict[vertex2] = [vertex2]
+            print(str(dict()))
 
 if __name__ == '__main__' :
